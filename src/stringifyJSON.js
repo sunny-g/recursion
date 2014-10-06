f2f5adefe8c2a0070f69aefa84bbc073ba902da7
sunny-g <sunny.gonna@gmail.com>
2014-10-06 13:19:20 -0500

    finished stringifyJSON completely

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,100 +1,56 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+/**
+ * Created by sunnyg on 10/6/14.
+ */
 
-// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-  /*
-  createString(s, thing)
-    if thing is obj
-      var count = 0
-      var size = _.size(thing)
-      for key in thing
-        s += '"' + key '":';
-        createString(s, thing[key])
-        if count != size - 1  // if this is not the last key in thing
-          s += ','
-        else
-          s += '}'
-    else
-      s += '"' + thing + '"'
-    return s
-  */
-  /*
-  test Obj and stack:
-  var o1 = { level1:
-            { level2:
-              { level3a: 'value3a',
-                level3b: 'value3b'
-              }
-            }
-          }
-  var o2 = { key1: 'value1' }
-  var o3 = { level1:
-            { level2 : 'value2' }
-           }
-  var str1 = "{"level1":{"level2":{"level3a":"value3a","level3b":"value3b"}}}"
-  var str2 = "{"key1":"value1"}"
-  var str3 = "{"level1":{"level2":"value2"}}"
-   */
 
-  // the functions
-  /*
-  function createString(str, thing2Stringify) {
-    // if thing2Stringify is object {
-    var count = 0;
-    _.each(thing2Stringify, function(value, key) {
-      if (Object.prototype.toString.call(value) === '[object Object]') {
-        str += '"' + key + '":';
-        createString(str, value);
-      } else {
-        str += '"' + key + '":"' + value.toString() + '"';
-        if (!(count === _.size(obj2Stringify) - 1)) {
-          count++;
-          str += ',';
-        } else {
-          str += '}';
-        }
-      }
-    });
-    return str;
-  }
-  */
+  var type = Object.prototype.toString.call(obj);
 
-  function createString(s, thing) {
-    var type = Object.prototype.toString.call(thing);
+  if (type === '[object Number]' || type === '[object Boolean]') {
+    return obj.toString();
+  } else if (type === '[object String]') {
+    return '"' + obj + '"';
+  } else if (type === '[object Null]') {
+    return '' + obj;
+  } else {
+    //////////////////////////
+    // OF TYPE ARRAY OR OBJECT
+    var s = '';
+
     if (type === '[object Object]') {
+      s += '{';
       var count = 0;
-      var size = _.size(thing);
-      for (var key in thing) {
-        s += '"' + key + '":' + createString(s, thing[key]);
-        if (count != size - 1) {  // if this is not the last key in thing
-          count++;
-          s += ','
-        } else {
-          s += '}'
+      _.each(obj, function(value, key) {
+        var valueType = Object.prototype.toString.call(value);
+        if (valueType !== '[object Function]' && valueType !== '[object Undefined]') {
+          s += '"' + key + '":' + stringifyJSON(value);
+          if (count < _.size(obj) - 1) {
+            count++;
+            s += ',';
+          }
         }
-      }
-    // TODO: A STRINGIFY FOR EACH TYPE
-    } else if (type === '[object Number]' ||
-      type === '[object String]' ||
-      type === '[object Array]') {
-      return thing.toString();
-    } else {
-      return thing;
+      });
+      s += '}';
+      return s;
+
+    } else if (type === '[object Array]') {
+      s += '[';
+      _.each(obj, function(e, i) {
+        s += stringifyJSON(e);
+        if (i < obj.length - 1) {
+          s += ',';
+        }
+      });
+      s += ']';
+      return s;
+
     }
-    return s
+
+
+
   }
 
-  return createString('{', obj);
 
-  /*
-  function obj2String(o) {
-    var str = '{';
-    _.each(o, function(value, key) {
-      str += '"' + key '":"'
-    })
-  }
-  */
+
 
 };

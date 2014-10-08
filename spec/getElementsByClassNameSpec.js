d24b18544c5f11972f4e6f2dbb25eeaa184a68ed
sunny-g <sunny.gonna@gmail.com>
2014-10-07 20:45:33 -0500

    finished recursion, parseJSON is left

--- spec/getElementsByClassNameSpec.js
+++ spec/getElementsByClassNameSpec.js
@@ -8,26 +8,27 @@ var htmlStrings = [
   '<p><div class="somediv"><div class="innerdiv"><span class="targetClassName">yay</span></div></div></p>'                      // 6
 ];
 
-/*  html strings
-0
-1
+// html strings
+/*
+0*
+1*
 2
-  p
+  p*
 3
-  p
-    p
+  p*
+    p*
 4
   p
   p
-    p
+    p*
 5
-  p
-  p
+  p*
+  p*
 6
   div
     div
-      span 'yay'
- */
+      span* 'yay'
+*/
 
 describe('getElementsByClassName', function(){
 
@@ -40,6 +41,7 @@ describe('getElementsByClassName', function(){
       var result = getElementsByClassName('targetClassName');
       var expectedNodeList = document.getElementsByClassName('targetClassName');
       var expectedArray = Array.prototype.slice.apply(expectedNodeList);
+
       var equality = _.isEqual(result, expectedArray); // why can't we use `===` here?
       expect(equality).to.equal(true);
 
--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,8 +5,73 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+  // builds and returns an array of DOM elements that have className
+  /*
+  we need to do 2 things:
+    touch every element recursively (depth-first seems natural)
+    if it.contains(className), then arr.push(it)
 
+  naive:
+    add every element to a flattened list
+    _.each(res, e.contains(className))
 
+  better
+    create func that recursively loads direct children nodes and gets each classList
+    checks if classList.contains(className) and if so, adds to list
+
+  the recursive function:
+    if elem has no children
+      push
+    else
+      for each child
+        recurse
+
+   */
+
+  // first attempt:
+  /*
+  var elementList = [];
+  var res = [];
+
+  var root = document.body;
+
+  function addElement(obj) {
+    if (obj.childNodes.length === 0 && obj.nodeName !== '#text') {
+      console.log('pushing: ' + obj);
+      elementList.push(obj)
+    } else {
+      _.each(obj, function(e, i) {
+        addElement(e);
+      })
+    }
+  }
+
+  addElement(root);
+  */
+
+  var res = [];
+
+  function recursiveSearch(elem) {
+    // checks if elem
+
+    var elemChildren = Array.prototype.slice.call(elem.children);
+    hasClass(elem);
+
+    if (elemChildren.length !== 0) {
+      _.each(elemChildren, function (child) {
+        recursiveSearch(child);
+      });
+    }
+  }
+
+  function hasClass(elem) {
+    if (elem.classList && elem.classList.contains(className)) {
+      res.push(elem);
+    }
+  }
+
+  recursiveSearch(document);
+
+  return res;
 
 };
